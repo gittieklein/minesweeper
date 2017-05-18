df26fdf90fc55f1082be012235d8bcbcbd6928dc
Gittie Klein <gittiekay@gmail.ciom>
2017-05-18 12:10:41 -0400

    change

--- src/MinesweeperGame.java
+++ src/MinesweeperGame.java
@@ -12,7 +12,7 @@ public class MinesweeperGame
 	public static void main(String args[]) 
 	{
 		//create a game data object - the same object will be used through out so that all classes have access to the same info
-		GameData gameData = GameData.getInstance(16, 30, 99);
+		GameData gameData = GameData.getInstance(9, 9, 10);
 		MinesweeperJFrame frame = new MinesweeperJFrame();
 		frame.setJMenuBar(createMenu());
 		frame.setVisible(true);
