8b596fbbb226f0487ce8538b0eae53664963db37
tamar neumann <tamar neumann>
2017-05-23 17:32:37 -0400

    worked on timer:

--- src/BelowComponent.java
+++ src/BelowComponent.java
@@ -18,6 +18,7 @@ public class BelowComponent extends JComponent
 		
 		GameData gameData = GameData.getInstance();
 		
+
 		//scale the image so you could set the size
 		mineLabel = new JLabel(gameData.getTotalMines() + " ");
 		JLabel mineIcon = new JLabel();
@@ -28,22 +29,20 @@ public class BelowComponent extends JComponent
 		mineIcon.setIcon(icon);
 	
 		mineLabel.setFont(new Font("Calibri", Font.PLAIN, 32));	//set font and size of text
-
 		
 		
 		//add number of mines and mine image to the component
 		this.add(mineLabel);
 		this.add(mineIcon);
 		
+	
 		 
 		 timerLabel = new JLabel();
 		 timerLabel.setFont(new Font("Calibri", Font.PLAIN, 32));
 		 
-		 //sets a margin to the right.
-		 timerLabel.setBorder(new EmptyBorder(0,0,0,250));
-
-		 this.add(timerLabel);
 		
+		 this.add(timerLabel);
+		 
 	}
 	
 	public static void startTimer()
@@ -69,7 +68,7 @@ public class BelowComponent extends JComponent
 		 
 		 Timer timer = new Timer(1000, new TimerListener());
 		 timer.start();
-			
+		
 	}
 	public static void editMines(int mines)
 	{
